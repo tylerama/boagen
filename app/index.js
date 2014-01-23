e757f2d2369dd76e9d697305167f79c1ce381a93
Tyler Lesperance <hi@iamtyler.ca>
2014-01-23 10:22:00 -0600

    Added less framework

--- app/index.js
+++ app/index.js
@@ -53,11 +53,41 @@ BoagenGenerator.prototype.app = function app() {
   this.mkdir('app/scripts');
   this.mkdir('app/styles');
   this.mkdir('app/styles/less');
+  this.mkdir('app/styles/less/pages');
   this.mkdir('app/fonts');
 
   this.copy('_package.json', 'package.json');
   this.copy('bowercc', 'bowercc.json');
   this.copy('_bower.json', 'bower.json');
+};
+
+BoagenGenerator.prototype.lessFramework = function lessFramework() {
+  this.copy('styles/less/testing.less', 'app/styles/less/testing.less');
+  this.copy('styles/less/variables.less', 'app/styles/less/variables.less');
+  this.copy('styles/less/colors.less', 'app/styles/less/colors.less');
+  this.copy('styles/less/scaffolding.less', 'app/styles/less/scaffolding.less');
+  this.copy('styles/less/master.less', 'app/styles/less/master.less');
+  this.copy('styles/less/images.less', 'app/styles/less/images.less');
+  this.copy('styles/less/svg.less', 'app/styles/less/svg.less');
+  this.copy('styles/less/image-grid.less', 'app/styles/less/image-grid.less');
+  this.copy('styles/less/shapes.less', 'app/styles/less/shapes.less');
+  this.copy('styles/less/buttons.less', 'app/styles/less/buttons.less');
+  this.copy('styles/less/lists.less', 'app/styles/less/lists.less');
+  this.copy('styles/less/classes.less', 'app/styles/less/classes.less');
+  this.copy('styles/less/helpers.less', 'app/styles/less/helpers.less');
+  this.copy('styles/less/reuse.less', 'app/styles/less/reuse.less');
+  this.copy('styles/less/typography.less', 'app/styles/less/typography.less');
+  this.copy('styles/less/links.less', 'app/styles/less/links.less');
+  this.copy('styles/less/mixins.less', 'app/styles/less/mixins.less');
+  this.copy('styles/less/elements.less', 'app/styles/less/elements.less');
+  this.copy('styles/less/grid.less', 'app/styles/less/grid.less');
+  this.copy('styles/less/normalize.less', 'app/styles/less/normalize.less');
+  this.copy('styles/less/form.less', 'app/styles/less/form.less');
+  this.copy('styles/less/print.less', 'app/styles/less/print.less');
+  this.copy('styles/less/lesshat.less', 'app/styles/less/lesshat.less');
+  this.copy('styles/less/pages/global.less', 'app/styles/less/pages/global.less');
+  this.copy('styles/less/pages/home.less', 'app/styles/less/pages/home.less');
+  this.copy('styles/less/pages/interior.less', 'app/styles/less/pages/interior.less');
 };
 
 // BoagenGenerator.prototype.dist = function dist() {
